12a9704c6aeb691272473d7e2ed417e6de88d82b
siriusBl4ck <sauravkale2019@gmail.com>
2021-12-05 21:58:17 +0530

    cleanup sbox

--- Saurav - SBox and Key Expansion/sbox/gf4_sq.v
+++
@@ -1,9 +0,0 @@
-function automatic [3:0] gf4_sq;
-    input [3:0] a;
-    begin
-    gf4_sq[3] = a[3];
-    gf4_sq[2] = a[1] ^ a[3];
-    gf4_sq[1] = a[2];
-    gf4_sq[0] = a[0] ^ a[2];
-    end
-endfunction
