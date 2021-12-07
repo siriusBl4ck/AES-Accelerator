22b32f8205adfd6001a33c73725611215f0dfe68
Ruban-VP <64304414+Ruban-VP@users.noreply.github.com>
2021-12-07 21:56:15 +0530

    Add files via upload

--- Ruban - AES Decryption/Addkey.v
+++ Ruban - AES Decryption/Addkey.v
@@ -2,7 +2,7 @@ module Addkey(
     input [127:0] a,
     input [127:0] b,
     output [127:0] out
-)
+);
 
 assign out = a^b;
 endmodule
--- Ruban - AES Decryption/InvMixCols.v
+++ Ruban - AES Decryption/InvMixCols.v
@@ -3,11 +3,10 @@ module InvMixCols(
     input [31:0] colin2,
     input [31:0] colin3,
     input [31:0] colin4,
-    output reg [31:0] colout1;
-    output reg [31:0] colout2;
-    output reg [31:0] colout3;
-    output reg [31:0] colout4;
+    output [127:0] out
 );
+
+reg [31:0] colout1, colout2, colout3, colout4;
 
 function automatic [7:0] mul2 (input [7:0] a);
     begin
@@ -15,38 +14,40 @@ function automatic [7:0] mul2 (input [7:0] a);
     end
 endfunction
 
-function automatic [7:0] mul (input [7:0] a, input [3:0] multiplier);   
+function automatic [7:0] mul (input [7:0] a, input [1:0] sel);   
     begin
-        case({multiplier[2],multiplier[1]})
-        2'b00: mul = mul2(mul2(mul2(a)))^a;
-        2'b01: mul = mul2(mul2(mul2(a))^a)^a;
-        2'b10: mul = mul2(mul2(mul2(a)^a))^a;
-        2'b11: mul = mul2(mul2(mul2(a)^a)^a);
+        case(sel)
+        2'b00: mul = mul2(mul2(mul2(a)))^a;          //x09
+        2'b01: mul = mul2(mul2(mul2(a))^a)^a;        //x0B
+        2'b10: mul = mul2(mul2(mul2(a)^a))^a;        //x0D
+        2'b11: mul = mul2(mul2(mul2(a)^a)^a);        //x0E
         endcase
     end
 endfunction
 
 always @(*)
 begin
-    colout1[31:24] = mul(colin1[31:24],4'hE)^mul(colin1[23:16],4'hB)^mul(colin1[15:8],4'hD)^mul(colin1[7:0],4'h9);
-    colout2[31:24] = mul(colin2[31:24],4'hE)^mul(colin2[23:16],4'hB)^mul(colin2[15:8],4'hD)^mul(colin2[7:0],4'h9);
-    colout3[31:24] = mul(colin3[31:24],4'hE)^mul(colin3[23:16],4'hB)^mul(colin3[15:8],4'hD)^mul(colin3[7:0],4'h9);
-    colout4[31:24] = mul(colin4[31:24],4'hE)^mul(colin4[23:16],4'hB)^mul(colin4[15:8],4'hD)^mul(colin4[7:0],4'h9);
+    colout1[31:24] = mul(colin1[31:24],2'b11)^mul(colin1[23:16],2'b01)^mul(colin1[15:8],2'b10)^mul(colin1[7:0],2'b00);
+    colout2[31:24] = mul(colin2[31:24],2'b11)^mul(colin2[23:16],2'b01)^mul(colin2[15:8],2'b10)^mul(colin2[7:0],2'b00);
+    colout3[31:24] = mul(colin3[31:24],2'b11)^mul(colin3[23:16],2'b01)^mul(colin3[15:8],2'b10)^mul(colin3[7:0],2'b00);
+    colout4[31:24] = mul(colin4[31:24],2'b11)^mul(colin4[23:16],2'b01)^mul(colin4[15:8],2'b10)^mul(colin4[7:0],2'b00);
 
-    colout1[23:16] = mul(colin1[31:24],4'h9)^mul(colin1[23:16],4'hE)^mul(colin1[15:8],4'hB)^mul(colin1[7:0],4'hD);
-    colout2[23:16] = mul(colin2[31:24],4'h9)^mul(colin2[23:16],4'hE)^mul(colin2[15:8],4'hB)^mul(colin2[7:0],4'hD);
-    colout3[23:16] = mul(colin3[31:24],4'h9)^mul(colin3[23:16],4'hE)^mul(colin3[15:8],4'hB)^mul(colin3[7:0],4'hD);
-    colout4[23:16] = mul(colin4[31:24],4'h9)^mul(colin4[23:16],4'hE)^mul(colin4[15:8],4'hB)^mul(colin4[7:0],4'hD);
+    colout1[23:16] = mul(colin1[31:24],2'b00)^mul(colin1[23:16],2'b11)^mul(colin1[15:8],2'b01)^mul(colin1[7:0],2'b10);
+    colout2[23:16] = mul(colin2[31:24],2'b00)^mul(colin2[23:16],2'b11)^mul(colin2[15:8],2'b01)^mul(colin2[7:0],2'b10);
+    colout3[23:16] = mul(colin3[31:24],2'b00)^mul(colin3[23:16],2'b11)^mul(colin3[15:8],2'b01)^mul(colin3[7:0],2'b10);
+    colout4[23:16] = mul(colin4[31:24],2'b00)^mul(colin4[23:16],2'b11)^mul(colin4[15:8],2'b01)^mul(colin4[7:0],2'b10);
 
-    colout1[15:8] = mul(colin1[31:24],4'hD)^mul(colin1[23:16],4'h9)^mul(colin1[15:8],4'hE)^mul(colin1[7:0],4'hB);
-    colout2[15:8] = mul(colin2[31:24],4'hD)^mul(colin2[23:16],4'h9)^mul(colin2[15:8],4'hE)^mul(colin2[7:0],4'hB);
-    colout3[15:8] = mul(colin3[31:24],4'hD)^mul(colin3[23:16],4'h9)^mul(colin3[15:8],4'hE)^mul(colin3[7:0],4'hB);
-    colout4[15:8] = mul(colin4[31:24],4'hD)^mul(colin4[23:16],4'h9)^mul(colin4[15:8],4'hE)^mul(colin4[7:0],4'hB);
+    colout1[15:8] = mul(colin1[31:24],2'b10)^mul(colin1[23:16],2'b00)^mul(colin1[15:8],2'b11)^mul(colin1[7:0],2'b01);
+    colout2[15:8] = mul(colin2[31:24],2'b10)^mul(colin2[23:16],2'b00)^mul(colin2[15:8],2'b11)^mul(colin2[7:0],2'b01);
+    colout3[15:8] = mul(colin3[31:24],2'b10)^mul(colin3[23:16],2'b00)^mul(colin3[15:8],2'b11)^mul(colin3[7:0],2'b01);
+    colout4[15:8] = mul(colin4[31:24],2'b10)^mul(colin4[23:16],2'b00)^mul(colin4[15:8],2'b11)^mul(colin4[7:0],2'b01);
 
-    colout1[7:0] = mul(colin1[31:24],4'hB)^mul(colin1[23:16],4'hD)^mul(colin1[15:8],4'h9)^mul(colin1[7:0],4'hE);
-    colout2[7:0] = mul(colin2[31:24],4'hB)^mul(colin2[23:16],4'hD)^mul(colin2[15:8],4'h9)^mul(colin2[7:0],4'hE);
-    colout3[7:0] = mul(colin3[31:24],4'hB)^mul(colin3[23:16],4'hD)^mul(colin3[15:8],4'h9)^mul(colin3[7:0],4'hE);
-    colout4[7:0] = mul(colin4[31:24],4'hB)^mul(colin4[23:16],4'hD)^mul(colin4[15:8],4'h9)^mul(colin4[7:0],4'hE);
+    colout1[7:0] = mul(colin1[31:24],2'b01)^mul(colin1[23:16],2'b10)^mul(colin1[15:8],2'b00)^mul(colin1[7:0],2'b11);
+    colout2[7:0] = mul(colin2[31:24],2'b01)^mul(colin2[23:16],2'b10)^mul(colin2[15:8],2'b00)^mul(colin2[7:0],2'b11);
+    colout3[7:0] = mul(colin3[31:24],2'b01)^mul(colin3[23:16],2'b10)^mul(colin3[15:8],2'b00)^mul(colin3[7:0],2'b11);
+    colout4[7:0] = mul(colin4[31:24],2'b01)^mul(colin4[23:16],2'b10)^mul(colin4[15:8],2'b00)^mul(colin4[7:0],2'b11);
 end
+
+assign out = {colout1, colout2, colout3, colout4};
 
 endmodule
