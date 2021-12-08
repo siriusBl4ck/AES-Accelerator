1814ef795677f190fb418758fe1a19a158250019
Surya Prasad S <69968227+GnosGnas@users.noreply.github.com>
2021-12-08 16:39:21 +0530

    Update AES_encrypt.v

--- AES outer modules/AES_encrypt.v
+++ AES outer modules/AES_encrypt.v
@@ -1,3 +1,81 @@
+
+module aes_encrypt (clk,
+	     reset,
+	     plaintext,
+		 start,
+		 key_len,
+		 ciphertext,
+		 ready,
+		 subkey,
+		 subkey_addr,
+		 subkey_valid
+		 );
+	input clk, reset, start;
+	input [127:0] plaintext;
+	input [2:0] key_len;
+	input [127:0] subkey;
+	input subkey_valid;
+	output [3:0] subkey_addr;
+	output [127:0] ciphertext;
+	output reg ready;
+
+	reg status;
+	reg [127:0] result;
+	reg [5:0] max_rounds; ////
+	reg [3:0] skey_addr;
+
+	wire [127:0] pre_addkey_out, subbytes_out, shiftrows_out, mixcols_out, addkey_in, addkey_out;
+	wire addkey_switch;
+	assign addkey_switch = (max_rounds==1);
+	assign addkey_in = addkey_switch? mixcols_out : shiftrows_out;
+
+	assign ciphertext = result;
+
+	//assign subkey_addr =  (|key_len) && (start) && (status==0)
+
+
+	AddRoundKey A1(pre_addkey_out, plaintext, subkey);
+
+	SubBytes SB(subbytes_out, result);
+	ShiftRows SR(shiftrows_out, subbytes_out);
+	MixColumns M(mixcols_out, shiftrows_out);
+	AddRoundKey A2(addkey_out, addkey_in, subkey);
+
+	always@(posedge clk) begin
+		if (reset) begin
+			status <= 0;
+			result <= 0;
+			max_rounds <= 0; 
+		end
+		else if ((|key_len) && (start) && (status==0))	begin
+			status <= 1;
+			if (key_len[2]) 	max_rounds <= 14;
+			else if (key_len[1]) max_rounds <= 12;
+			else max_rounds <= 10;
+			result <= pre_addkey_out;
+			skey_addr <= 0;
+		end
+		else if (status == 1) begin
+			if (subkey_valid) begin
+				result <= pre_addkey_out;
+				status <= 2;
+				skey_addr <= skey_addr+1;
+			end
+		end
+		else if (status == 2) begin
+			max_rounds <= max_rounds-1;
+			result <= addkey_out;
+			skey_addr <= skey_addr+1;
+			
+			if (max_rounds==1) begin
+				status <= 0;
+				ready <= 1'b1;
+			end
+		end
+	end
+
+endmodule
+
 module SubBytes (res, inp);
 	input [127:0] inp;
 	output [127:0] res;
@@ -206,63 +284,3 @@ module AddRoundKey (res, inp, subkey);
 	assign res = inp ^ subkey;
 endmodule
 
-module aes_encrypt (clk,
-	     reset,
-	     plaintext,
-		 start,
-		 key_len,
-		 ciphertext,
-		 ready,
-		 subkey,
-		 subkey_addr
-		 );
-	input clk, reset, start;
-	input [127:0] plaintext;
-	input [2:0] key_len;
-	input [127:0] subkey;
-	output [3:0] subkey_addr;
-	output [127:0] ciphertext;
-	output reg ready;
-
-	reg status;
-	reg [127:0] result;
-	reg [5:0] max_rounds; ////
-
-	wire [127:0] pre_addkey_out, subbytes_out, shiftrows_out, mixcols_out, addkey_in, addkey_out;
-	wire addkey_switch;
-	assign addkey_switch = (max_rounds==1);
-	assign addkey_in = addkey_switch? mixcols_out : shiftrows_out;
-
-	assign ciphertext = result;
-
-	AddRoundKey A1(pre_addkey_out, plaintext, subkey);
-
-	SubBytes SB(subbytes_out, result);
-	ShiftRows SR(shiftrows_out, subbytes_out);
-	MixColumns M(mixcols_out, shiftrows_out);
-	AddRoundKey A2(addkey_out, addkey_in, subkey);
-
-	always@(posedge clk) begin
-		if (reset) begin
-			status <= 0;
-			result <= 0;
-			max_rounds <= 0; 
-		end
-		else if ((|key_len) && (start) && (status==0))	begin
-			status <= 1;
-			if (key_len[2]) 	max_rounds <= 14;
-			else if (key_len[1]) max_rounds <= 12;
-			else max_rounds <= 10;
-			result <= pre_addkey_out;
-		end
-		else if (status==1) begin
-			max_rounds <= max_rounds-1;
-			result <= addkey_out;
-			if (max_rounds==1) begin
-				status <= 0;
-				ready <= 1'b1;
-			end
-		end
-	end
-
-endmodule
