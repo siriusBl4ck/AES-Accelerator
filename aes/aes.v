af24e203a29d4c3f68dcd761c8d48d389ec8c2ad
siriusBl4ck <sauravkale2019@gmail.com>
2021-12-08 17:41:51 +0530

    cpu version

--- aes/aes.v
+++ aes/aes.v
@@ -1,11 +1,12 @@
 module aes(
-    input [127:0] plaintxt_encrypt,
-    output [127:0] ciphertxt_encrypt,
+    input [127:0] plaintext_encrypt,
+    output [127:0] ciphertext_encrypt,
     input en_encrypt,
     output rdy_encrypt,
-    input [127:0] ciphertxt_decrypt,
-    output [127:0] plaintxt_decrypt,
+    input [127:0] ciphertext_decrypt,
+    output [127:0] plaintext_decrypt,
     input en_decrypt,
-    output rdy_decrypt
+    output rdy_decrypt,
+    input [255:0] short_key
 );
 endmodule
