60cbc0471a984db2e6ad4f12e9e3c9d4a8800f09
Surya Prasad S <69968227+GnosGnas@users.noreply.github.com>
2021-12-08 17:17:06 +0530

    Update AES_encrypt.v

--- AES outer modules/AES_encrypt.v
+++ AES outer modules/AES_encrypt.v
@@ -15,14 +15,13 @@ module aes_encrypt (clk,
 	input [2:0] key_len;
 	input [127:0] subkey;
 	input subkey_valid;
-	output [3:0] subkey_addr;
+	output reg [3:0] skey_addr;
 	output [127:0] ciphertext;
 	output reg ready;
 
-	reg status;
+	reg [1:0] status;
 	reg [127:0] result;
 	reg [5:0] max_rounds; ////
-	reg [3:0] skey_addr;
 
 	wire [127:0] pre_addkey_out, subbytes_out, shiftrows_out, mixcols_out, addkey_in, addkey_out;
 	wire addkey_switch;
@@ -30,9 +29,6 @@ module aes_encrypt (clk,
 	assign addkey_in = addkey_switch? mixcols_out : shiftrows_out;
 
 	assign ciphertext = result;
-
-	//assign subkey_addr =  (|key_len) && (start) && (status==0)
-
 
 	AddRoundKey A1(pre_addkey_out, plaintext, subkey);
 
@@ -46,14 +42,15 @@ module aes_encrypt (clk,
 			status <= 0;
 			result <= 0;
 			max_rounds <= 0; 
+			ready <= 0;
 		end
 		else if ((|key_len) && (start) && (status==0))	begin
 			status <= 1;
-			if (key_len[2]) 	max_rounds <= 14;
-			else if (key_len[1]) max_rounds <= 12;
-			else max_rounds <= 10;
-			result <= pre_addkey_out;
+			if (key_len[2]) max_rounds <= 15;
+			else if (key_len[1]) max_rounds <= 13;
+			else max_rounds <= 11;
 			skey_addr <= 0;
+			ready <= 0;
 		end
 		else if (status == 1) begin
 			if (subkey_valid) begin
@@ -63,13 +60,15 @@ module aes_encrypt (clk,
 			end
 		end
 		else if (status == 2) begin
-			max_rounds <= max_rounds-1;
-			result <= addkey_out;
-			skey_addr <= skey_addr+1;
-			
-			if (max_rounds==1) begin
-				status <= 0;
-				ready <= 1'b1;
+			if (subkey_valid) begin
+				max_rounds <= max_rounds-1;
+				result <= addkey_out;
+				skey_addr <= skey_addr+1;
+
+				if (max_rounds==1) begin
+					status <= 0;
+					ready <= 1'b1;
+				end
 			end
 		end
 	end
