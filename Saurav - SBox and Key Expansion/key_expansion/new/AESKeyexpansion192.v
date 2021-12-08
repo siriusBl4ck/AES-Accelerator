c467dabd77ecc26f354988d4dc17af8dfd612a21
Surya Prasad S <69968227+GnosGnas@users.noreply.github.com>
2021-12-08 11:13:15 +0530

    Update AESKeyexpansion192.v

--- Saurav - SBox and Key Expansion/key_expansion/new/AESKeyexpansion192.v
+++ Saurav - SBox and Key Expansion/key_expansion/new/AESKeyexpansion192.v
@@ -21,7 +21,6 @@ module key_exp_tb;
     reg [191:0] short_key;
     wire [127:0] subkey;
     wire rdy;
-    reg [127:0] res;
     
     AESKeyexpansion_192 s1(clk, reset, start, short_key, subkey, rdy);
 
@@ -64,18 +63,17 @@ module AESKeyexpansion_192(
     output valid_skey
 );
 
-	reg [5:0] i;  //last bit always 0 so can be removed!
+	reg [4:0] i;  //last bit always 0 so can be removed!
 	reg [191:0] prev_period_key_1; 
 	reg status;
 
 	wire [31:0] key1, key2, key3, key4;
 
-	current_word_gen_192 word1(.i(i),.prev_word(prev_period_key_1[31:0]),.prev_period_word(prev_period_key_1[191:160]),.current_word(key1));
-	current_word_gen_192 word2(.i(i+6'd1),.prev_word(key1),.prev_period_word(prev_period_key_1[159:128]),.current_word(key2));
-	current_word_gen_192 word3(.i(i+6'd2),.prev_word(key2),.prev_period_word(prev_period_key_1[127:96]),.current_word(key3));
-	current_word_gen_192 word4(.i(i+6'd3),.prev_word(key3),.prev_period_word(prev_period_key_1[95:64]),.current_word(key4));
-
-	assign subkey = (i==6)? ({prev_period_key_1[63:0], key1, key2}): ({key1, key2, key3, key4});
+	current_word_gen_192 word1(.i({i,1'd0}),.prev_word(prev_period_key_1[31:0]),.prev_period_word(prev_period_key_1[191:160]),.current_word(key1));
+	current_word_gen_192 word2(.i({i,1'd1}),.prev_word(key1),.prev_period_word(prev_period_key_1[159:128]),.current_word(key2));
+	current_word_gen_192 word3(.i({i[4:1],2'd2}),.prev_word(key2),.prev_period_word(prev_period_key_1[127:96]),.current_word(key3));
+	current_word_gen_192 word4(.i({i[4:1],2'd3}),.prev_word(key3),.prev_period_word(prev_period_key_1[95:64]),.current_word(key4));
+	assign subkey = (i==3)? ({prev_period_key_1[63:0], key1, key2}): ({key1, key2, key3, key4}); //6
 	assign valid_skey = status;
 
 	always @(posedge clk)
@@ -87,21 +85,21 @@ module AESKeyexpansion_192(
 		end
 		else begin
 			if (start) begin
-				i <= 6;
+				i <= 3; //6
 				prev_period_key_1 <= short_key;
 				status <= 1;
 			end
-			else if (i==6) begin
-				i <= 8;
+			else if (i==3) begin //6
+				i <= 4; //8
 				prev_period_key_1 = {prev_period_key_1[127:0],key1,key2};
 				status <= 1;
 			end
-			else if(i<48) begin
-				i <= i+4;
+			else if(i<24) begin //48
+				i <= i+2; //+4
 				prev_period_key_1 = {prev_period_key_1[63:0],key1,key2,key3,key4};
 				status <= 1;
 			end
-			else if(i==48) begin
+			else if(i==24) begin //48
 				i <= 0;
 				status <= 0;
 				prev_period_key_1 <= 0;
